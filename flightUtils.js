95767d8b18441805cf986b458c96712bea723561
aegorsuch <alex@weartak.com>
2026-03-09 15:33:28 -0400

    refactor: remove unused parseGateFromTitle_ function from flightUtils.js

--- flightUtils.js
+++ flightUtils.js
@@ -1,10 +1,4 @@
 // Pure utility functions for Node.js testing
 
-function parseGateFromTitle_(title) {
-  const match = /\bat\s+gate\s+([A-Z0-9-]+)/i.exec(title || "");
-  return match ? match[1].toUpperCase() : "";
-}
 
-module.exports = {
-  parseGateFromTitle_,
-};
+// ...existing code...
